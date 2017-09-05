27befc00342da8efc9d0c1f0d1aeb1006b678844
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-09-05 21:46:37 +0800

    lock files

---
+++ clean_script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+watchman watch-del-all 1>/dev/null
+
+rm -rf node_modules 1>/dev/null
+rm -rf yarn.lock  1>/dev/null
+rm -rf package-lock.json 1>/dev/null
+rm -rf $TMPDIR/react-packager-* 1>/dev/null
+rm -rf ios/build 1>/dev/null
+
+npm cache clear --force -s 1>/dev/null
+npm cache verify 1>/dev/null
+
+# yarn
+# yarn add react-native-fbsdk@0.6.0
+#react-native link
+
+yarn
+npm i
