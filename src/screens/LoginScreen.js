ec519250a885f63d6b61023a780d73aa895dd4ef
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-09-08 14:01:28 +0800

    styling login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,16 +2,29 @@
 
 import React, {Component} from 'react';
 
-import {ActivityIndicatorIOS, StyleSheet, View} from 'react-native';
-import LoginManager from "../managers/LoginManager"
+import {ActivityIndicatorIOS, Button, Image, StyleSheet, Text, View} from 'react-native';
 import * as appActions from "../actions/appActions"
 import {connect} from 'react-redux';
+import {LoginManager} from 'react-native-fbsdk';
 
-const FBSDK = require('react-native-fbsdk');
-const {
-    LoginButton, AccessToken
-} = FBSDK;
 
+// class UIColor
+// def self.gs_green
+// '#40e7bb'.to_color
+// end
+//
+// def self.gs_blue
+// '#408BE7'.to_color
+// end
+//
+// def self.gs_separatorColor
+// '#E5E5E5'.to_color
+// end
+//
+// def self.gs_grayText
+// '#BBBBBB'.to_color
+// end
+// end
 
 class Login extends Component {
 
@@ -21,11 +34,33 @@ class Login extends Component {
 
     render() {
         return (
-            <View>
-                <LoginButton
-                    onLoginFinished={this.onLoginFinished.bind(this)}
-                />
-            </View>
+            <Image
+                source={require('../img/welcome_screen.jpg')}
+                style={{
+                    flex: 1,
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    justifyContent: 'center',
+                }}
+            >
+                <View
+                    style={{
+                        alignItems: 'center'
+                    }}
+                >
+                    <Text>forget bla bla</Text>
+                    <Text>Restaurant bla bla</Text>
+                    <Image source={require('../img/goodshit_beta.png')}/>
+                    <Button
+                        onPress={this.handleFacebookLogin.bind(this)}
+                        title="Sign in with Facebook"
+                        color="#40e7bb"
+                        accessibilityLabel="Sign in with Facebook"
+                    />
+                    <Text>Don't publish blah blah</Text>
+                </View>
+            </Image>
         );
     }
 
@@ -40,6 +75,23 @@ class Login extends Component {
             //this.performLogin();
             this.props.dispatch(appActions.login());
         }
+    }
+
+    handleFacebookLogin () {
+        LoginManager.logInWithReadPermissions(['public_profile', 'email', 'user_friends'])
+            .then(
+                result => {
+                    if (result.isCancelled) {
+                        console.log('Login cancelled')
+                    } else {
+                        console.log('Login success with permissions: ' + result.grantedPermissions.toString());
+                        this.props.dispatch(appActions.login());
+                    }
+                },
+                error => {
+                    console.log('Login fail with error: ' + error)
+                }
+            )
     }
 }
 
