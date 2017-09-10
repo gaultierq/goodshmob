e3751f8064af7618e703db90e8881eaebb8da21f
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-09-10 10:01:52 +0800

    list stub

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,26 +1,74 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Button, Text} from 'react-native';
+import {StyleSheet, View, Button, Text, FlatList} from 'react-native';
 import  * as activitesActions from '../actions/activitiesActions'
 import {connect} from "react-redux";
 
 class MainScreen extends Component {
+
+
+    _keyExtractor = (item, index) => item.id;
+
 
     constructor(){
         super();
     }
 
     componentDidMount() {
+        this.fetch();
+    }
+
+    fetch() {
         this.props.dispatch(activitesActions.fetchActivities());
     }
 
+    renderItem(item) {
+        let activity = item.item;
+        return <ActivityItem
+            id={activity.id}
+            onPressItem={this.onPressItem}
+            title={`bing ${activity.id}`}
+        />
+    }
+
+    onPressItem(id) {
+        alert(`coucou ${id}`);
+    }
+
     render() {
+        let activities = this.props.activities.activities || [{id: 35}];
         return (
             <View style={styles.container}>
-                <Text>
-                    { `activities: ${JSON.stringify(this.props.activities)}`}
-                </Text>
+                <Button
+                    title="fetch"
+                    onPress={this.fetch.bind(this)}
+                />
+
+                <FlatList style={styles.list}
+                    data={activities}
+                    renderItem={this.renderItem}
+                    keyExtractor={this._keyExtractor}
+                />
+                {/*<Text>*/}
+                    {/*{ `activities ${activities.length}: ${JSON.stringify(activities)}`}*/}
+                {/*</Text>*/}
             </View>
         );
+    }
+}
+
+class ActivityItem extends React.Component {
+    _onPress = () => {
+        this.props.onPressItem(this.props.id);
+    };
+
+    render() {
+        return (
+            <Text
+                onPress={this._onPress}
+            >
+                {`heeeey: ${this.props.title}`}
+            </Text>
+        )
     }
 }
 
