39fc60f243fb9b7dfc600f4184dbb92cbd921b10
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-10-31 16:50:51 +0800

    test++

--- src/app.js
+++ src/app.js
@@ -86,15 +86,15 @@ export default class App {
     // testScreen = {
     //     screen: {
     //         label: 'test',
-    //         screen: 'goodsh.ShareScreen',
+    //         screen: 'goodsh.CommunityScreen',
     //     },
     //     passProps: {
-    //         item: {
-    //             id: "8ab94a3c-43b2-4e5c-acfb-d4ff268f93b1",
-    //             title: "test_title",
-    //             url: "test_url"
-    //
-    //         },
+    //         // item: {
+    //         //     id: "8ab94a3c-43b2-4e5c-acfb-d4ff268f93b1",
+    //         //     title: "test_title",
+    //         //     url: "test_url"
+    //         // },
+    //         userId: "662a61d0-5473-4d09-9410-c63aadc12e6c"
     //     }
     // };
 
