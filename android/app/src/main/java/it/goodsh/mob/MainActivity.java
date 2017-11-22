1c1bfb149e411eabe8745def3c3a20c724045da2
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-11-22 15:36:38 +0800

    android: splash screen

--- android/app/src/main/java/it/goodsh/mob/MainActivity.java
+++ android/app/src/main/java/it/goodsh/mob/MainActivity.java
@@ -4,6 +4,21 @@ import com.facebook.react.ReactActivity;
 import android.content.Intent;
 import com.reactnativenavigation.controllers.SplashActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.Dimension;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
+
 public class MainActivity extends SplashActivity {
 
     /**
@@ -20,4 +35,28 @@ public class MainActivity extends SplashActivity {
         super.onActivityResult(requestCode, resultCode, data);
         MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
+
+        @Override
+        public View createSplashLayout() {
+
+            LinearLayout view = new LinearLayout(this);
+            view.setGravity(Gravity.CENTER);
+
+
+            Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.splash1);
+            BitmapDrawable bitmapDrawable = new BitmapDrawable(bmp);
+            bitmapDrawable.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+
+            view.setBackgroundDrawable(bitmapDrawable);
+
+            ImageView goodsh = new ImageView(this);
+            goodsh.setImageResource(R.drawable.goodsh2);
+            int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 180, getResources().getDisplayMetrics());
+
+            goodsh.setLayoutParams(new FrameLayout.LayoutParams(width, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+            view.addView(goodsh);
+            return view;
+        }
 }
