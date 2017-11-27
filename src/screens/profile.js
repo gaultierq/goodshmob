3ec598d2324c3583b8a802400082a42172dfdafa
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-11-27 19:27:36 +0800

    feedback ++

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -3,9 +3,19 @@
 
 import React, {Component} from 'react';
 
-import {ActivityIndicator, Button, Image, ImageBackground, StyleSheet, Text, View, TextInput} from 'react-native';
+import {
+    ActivityIndicator,
+    Button,
+    Image,
+    ImageBackground,
+    Linking,
+    StyleSheet,
+    Text,
+    TextInput,
+    View
+} from 'react-native';
 import {connect} from 'react-redux';
-import type {Id, User} from "../types";
+import type {Id} from "../types";
 import {buildData, dataStateToProps} from "../utils/DataUtils";
 import ApiAction from "../utils/ApiAction";
 import * as Api from "../utils/Api";
@@ -13,9 +23,9 @@ import * as authActions from '../auth/actions'
 import {currentUserId} from "../CurrentUser";
 import i18n from '../i18n/i18n'
 import * as UI from "./UIStyles";
-import {renderSimpleLink} from "./UIStyles";
-import {renderLink} from "./UIStyles";
-import {renderSimpleButton} from "./UIStyles";
+import {renderLink, renderSimpleButton} from "./UIStyles";
+import SmartInput from "./components/SmartInput";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 type Props = {
     // userId: Id,
@@ -57,6 +67,7 @@ class Profile extends Component<Props, State> {
         let notEditable = false;
 
         return (
+
             <ImageBackground
                 source={require('../img/welcome_screen.jpg')}
                 style={{
@@ -67,56 +78,69 @@ class Profile extends Component<Props, State> {
                     justifyContent: 'center',
                 }}
             >
-
-
-                {/*user*/}
-                <View style={{
-                    alignItems: 'center',
-                }}>
-
-                    <Image style={{marginBottom: 20}} source={require('../img/goodsh.png')}/>
-                    <Text style={{fontFamily: 'Chivo', marginBottom: 20, fontSize: 14, fontWeight: 'bold'}}>{i18n.t('profile_screen.title', {love: i18n.t('profile_screen.love')})}</Text>
-                    <Text style={{fontSize: 14, textAlign: "center"}}>{i18n.t('profile_screen.subtitle')}</Text>
-
-
-                    <TextInput
-                        editable={!notEditable}
-                        onSubmitEditing={this.createFeedback.bind(this)}
-                        value={this.state.feedback}
-                        multiline
-                        onChangeText={(feedback) => this.setState({feedback})}
-                        placeholder={i18n.t('profile_screen.feedback_textfield.placeholder')}
-                        style={[
-                            styles.input,
-                            (notEditable ? {color: "grey"} : {color: "black"}),
-                            {margin: 20}
-                        ]}
-                    />
-
+                <KeyboardAwareScrollView
+                    scrollEnabled={false}
+                >
                     <View style={{
-                        flexDirection: 'row',
                         alignItems: 'center',
-                    }}
-                    >
-                        {user && user.image && <Image source={{uri: user.image}}
-                                                      style={styles.userAvatar}
-                        />}
-                        {user && <Text style={styles.userName}>{user.firstName + " " + user.lastName}</Text>}
+                    }}>
+
+                        <Image style={{marginBottom: 20}} source={require('../img/goodsh.png')}/>
+                        <Text style={{fontFamily: 'Chivo', marginBottom: 20, fontSize: 14, fontWeight: 'bold'}}>{i18n.t('profile_screen.title', {love: i18n.t('profile_screen.love')})}</Text>
+                        <Text style={{fontSize: 14, textAlign: "center"}}>{i18n.t('profile_screen.subtitle')}</Text>
+
+
+                        <SmartInput
+                            containerStyle={{padding: 6}}
+                            inputStyle={{fontSize: 15}}
+                            // inputContainerStyle={{borderRadius: 1}}
+                            execAction={(input: string) => this.sendFeedback(input)}
+                            placeholder={"profile_screen.feedback_textfield.placeholder"}
+                            multiline
+                            height={150}
+                        />
+
+                        <View style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                        }}
+                        >
+                            {user && user.image && <Image source={{uri: user.image}}
+                                                          style={styles.userAvatar}
+                            />}
+                            {user && <Text style={styles.userName}>{user.firstName + " " + user.lastName}</Text>}
+
+                        </View>
+
+                        {renderSimpleButton("logout", this.logout.bind(this))}
+
+                        {renderLink("Terms", "https://goodsh.it/terms")}
 
                     </View>
-
-                    {renderSimpleButton("logout", this.logout.bind(this))}
-
-                    {renderLink("Terms", "https://goodsh.it/terms")}
-
-                </View>
+                </KeyboardAwareScrollView>
             </ImageBackground>
+
         );
     }
 
 
-    createFeedback() {
+    sendFeedback(input: string) {
+        return new Promise((resolve, reject)=> {
 
+            let url = `mailto:feedback@goodsh.it?subject=GOODSH' Feedback&body=${input}`;
+            Linking.canOpenURL(url).then(supported => {
+                if (supported) {
+                    Linking.openURL(url);
+                    resolve();
+                } else {
+                    let message = "Don't know how to open URI: " + url;
+                    console.log(message);
+                    reject(message);
+                }
+            });
+
+            resolve();
+        });
     }
 
     logout() {
