a5a38e888f02d27393377321a41b4dd45ac60ca3
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-12-03 22:36:16 +0800

    clean script ++

--- clean_script.sh
+++ clean_script.sh
@@ -7,17 +7,7 @@ rm -rf yarn.lock  1>/dev/null
 rm -rf package-lock.json 1>/dev/null
 rm -rf $TMPDIR/react-packager-* 1>/dev/null
 rm -rf ios/build 1>/dev/null
+rm -rf ios/Pod 1>/dev/null
 
 npm cache clear --force -s 1>/dev/null
 npm cache verify 1>/dev/null
-
-# yarn
-# yarn add react-native-fbsdk@0.6.0
-#react-native link
-
-yarn
-npm i
-yarn
-npm i
-yarn
-npm i
