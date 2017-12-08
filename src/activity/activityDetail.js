0474908480f52f3ffd1a30529ec640dbf155ad82
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-12-08 18:44:49 +0800

    fix crash

--- src/activity/activityDetail.js
+++ src/activity/activityDetail.js
@@ -120,13 +120,16 @@ class ActivityDetailScreen extends Screen<Props, State> {
     goBuy(activity: Activity) {
 
         let url = _.get(activity, 'resource.location') || _.get(activity, 'resource.url');
-        Linking.canOpenURL(url).then(supported => {
-            if (supported) {
-                Linking.openURL(url);
-            } else {
-                console.log("Don't know how to open URI: " + url);
-            }
-        });
+        if (url) {
+            Linking.canOpenURL(url).then(supported => {
+                if (supported) {
+                    Linking.openURL(url);
+                } else {
+                    console.log("Don't know how to open URI: " + url);
+                }
+            });    
+        }
+        
 
     }
 
