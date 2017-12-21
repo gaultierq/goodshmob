eea2f84ef4b3282e0052da6bcc18a6d0763b8cdf
Quentin Gaultier <quentin.gaultier@gmail.com>
2017-12-21 22:45:01 +0800

    rights

---
+++ src/ui/rights.js
@@ -0,0 +1,31 @@
+// @flow
+
+
+import type {Activity} from "../types";
+import {sanitizeActivityType} from "../helpers/DataUtils";
+
+export default class ActionRights {
+
+    activity: Activity;
+
+    constructor(activity: Activity) {
+        this.activity = activity;
+    }
+
+    canBuy() {
+        let resource = this.activity.resource;
+        return resource && sanitizeActivityType(resource.type) === 'creativeWorks';
+    }
+
+    canLike() {
+        return !this.liked();
+    }
+
+    liked() {
+        return this.activity.meta && this.activity.meta["liked"];
+    }
+
+    canUnlike() {
+        return this.liked();
+    }
+}
