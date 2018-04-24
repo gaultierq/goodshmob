4dee9a5c4a8e08f9c799f843b4d0fc595ccc0ac3
Quentin Gaultier <quentin.gaultier@gmail.com>
2018-04-24 21:48:49 +0200

    clean android cache

--- clean_script.sh
+++ clean_script.sh
@@ -6,6 +6,8 @@ rm -rf node_modules 1>/dev/null
 rm -rf $TMPDIR/react-packager-* 1>/dev/null
 rm -rf ios/build 1>/dev/null
 rm -rf ios/Pod 1>/dev/null
+rm -rf android/build 1>/dev/null
+rm -rf android/app/build 1>/dev/null
 
 npm cache clear --force -s 1>/dev/null
 npm cache verify 1>/dev/null
