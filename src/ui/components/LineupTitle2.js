7eaabf4b9de7c0f3df4e566109eb16c2e007fd12
EliotAndres <eliot.andres@eleves.enpc.fr>
2018-05-24 17:08:04 +0100

    Update medals display for Android.

--- src/ui/components/LineupTitle2.js
+++ src/ui/components/LineupTitle2.js
@@ -65,17 +65,14 @@ export default class LineupTitle2 extends Component<Props, State> {
                             }}>
                                 <Text style={STYLES.SECTION_TITLE}>
                                     {lineup.name}
+                                    {' '}
 
                                     {
-                                        __IS_IOS__ && this.getMedals(lineup)
+                                        this.getMedals(lineup)
                                     }
 
                                 </Text>
                             </View>
-
-                            {
-                                __IS_ANDROID__ && this.getMedals(lineup)
-                            }
 
                             {
                                 children
@@ -121,8 +118,22 @@ export default class LineupTitle2 extends Component<Props, State> {
     renderMedal(count: number, icon: string, displayDot: () => boolean, color: Color = Colors.greyish) {
         const iconSize = 15;
 
+        if (__IS_ANDROID__) {
+            return count > 0 && <Text style={[styles.medalsContainer, {paddingLeft: 10, marginLeft: 10}]} key={icon}>
+                {icon === 'th-large' && <Text style={[styles.smallText, {marginLeft: 4, color,
+                    alignSelf: 'flex-end',
+                    // backgroundColor: 'red',
+                }]}>({count})</Text>}
+                {icon === 'star' && <Text style={[styles.smallText, {color, marginHorizontal: 6}]}>★</Text>}
+                {icon === 'star' && <Text style={[styles.smallText, {marginLeft: 4, color,
+                    alignSelf: 'flex-end',
+                    // backgroundColor: 'red',
+                }]}>{count}</Text>}
+            </Text>;
+        }
+
         return count > 0 && <View style={[styles.medalsContainer, {marginLeft: 4}]} key={icon}>
-            {displayDot.next().value && <Text style={[styles.smallText, {color, marginHorizontal: 6}]}>•</Text>}
+            {displayDot.next().value && <Text style={[styles.smallText, {color, marginHorizontal: 6}]}>• </Text>}
             <Icon name={icon} size={iconSize} color={color}/>
             <Text style={[styles.smallText, {marginLeft: 4, color,
                 alignSelf: 'flex-end',
