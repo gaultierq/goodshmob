a8313ac817ffd4d645bfe50d3b1c50867ba4efc6
Quentin Gaultier <quentin.gaultier@gmail.com>
2018-05-28 13:12:02 +0200

    fix crash on logout

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,4 @@
 
  export const currentUserFilter = function (state, props) {
-    const userId = props.userId
-    if (!userId) throw "please provide a userId to this selector"
-    return _.get(state, `data.users.${userId}`)
+     return _.get(state, `data.users.${props.userId}`)
 }
