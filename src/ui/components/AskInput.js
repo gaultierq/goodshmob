a93a8a0ae5ed811525f2c805c264a977f077459a
Quentin Gaultier <quentin.gaultier@gmail.com>
2018-06-15 12:28:54 +0200

    ask input design ++

--- src/ui/components/AskInput.js
+++ src/ui/components/AskInput.js
@@ -1,30 +1,36 @@
 // @flow
-import React, {Component} from 'react';
-import {Alert, Clipboard, KeyboardAvoidingView, Dimensions, Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
-import type {Ask, Id, ItemType, RequestState} from "../../types";
-import {CheckBox} from "react-native-elements";
-import {connect} from "react-redux";
-import {logged} from "../../managers/CurrentUser"
-import * as Api from "../../managers/Api";
-import ApiAction from "../../helpers/ApiAction";
+import React, {Component} from 'react'
+import {
+    Alert,
+    Clipboard,
+    Dimensions,
+    Image,
+    KeyboardAvoidingView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from 'react-native'
+import type {RequestState} from "../../types"
+import {CheckBox} from "react-native-elements"
+import {connect} from "react-redux"
+import {currentUser, logged} from "../../managers/CurrentUser"
+import * as Api from "../../managers/Api"
+import ApiAction from "../../helpers/ApiAction"
 
 import _Messenger from "../../managers/Messenger"
-import {Colors} from "../colors";
-import {SFP_TEXT_BOLD} from "../fonts";
-import type {PendingAction} from "../../helpers/ModelUtils";
-import {pendingActionWrapper} from "../../helpers/ModelUtils";
-import {renderSimpleButton} from "../UIStyles";
-import {
-    actions as userActions,
-    actionTypes as userActionTypes
-} from "../../redux/UserActions"
+import {Colors} from "../colors"
+import {SFP_TEXT_BOLD} from "../fonts"
+import {renderSimpleButton} from "../UIStyles"
+import {Avatar, registerLayoutAnimation, scheduleOpacityAnimation} from "../UIComponents"
 
 type Props = {
 };
 
 type State = {
+    isFocused?: boolean,
     askContent?: string,
-    isAsking?: boolean,
     reqCreateAsk?: RequestState,
 };
 
@@ -37,14 +43,19 @@ export default class AskInput extends Component<Props, State> {
     render() {
 
         let askContent = this.state.askContent;
-        let buttonDisabled = this.state.isAsking || !askContent;
+        const asking = this.state.reqCreateAsk === 'sending'
+        let buttonDisabled = asking || !askContent;
         const ok = this.state.reqCreateAsk === 'ok'
-        const notEditable = this.state.isAsking || ok;
+        const notEditable = asking || ok;
 
 
-        return (
-            <View style={{backgroundColor: Colors.green, paddingHorizontal: 10, paddingTop: 10}}>
+        const buttonCollor = Colors.greyish
+        return <View style={{padding: 10}}>
 
+            <View style={{
+                // backgroundColor: Colors.white,
+                flexDirection: 'row', borderBottomWidth: StyleSheet.hairlineWidth, borderColor: Colors.greyish}}>
+                <Avatar user={currentUser()}/>
                 <TextInput
                     editable={!notEditable}
                     onSubmitEditing={this.createAsk.bind(this)}
@@ -53,49 +64,62 @@ export default class AskInput extends Component<Props, State> {
                     numberOfLines={6}
                     maxLength={200}
                     blurOnSubmit
+                    onFocus={()=> {this.setFocused(true)}}
+                    onBlur={()=> {this.setFocused(false)}}
                     onChangeText={(askContent) => this.setState({askContent})}
                     placeholder={i18n.t("actions.ask_friend")}
-                    placeholderTextColor={"rgba(255,255,255,0.6)"}
-                    autoFocus={false}
-                    textAlignVertical={'top'}
-                    selectionColor={'transparent'}
+                    // placeholderTextColor={"rgba(255,255,255,0.6)"}
+                    // textAlignVertical={'top'}
+                    // selectionColor={'transparent'}
                     style={[
                         styles.input,
-                        (notEditable ? {backgroundColor: Colors.transparent} : {backgroundColor: Colors.darkerGreen}),
+                        {opacity: (notEditable ? 0.5 : 1)},
+                        {minHeight: 50}
                     ]}
                     returnKeyType={'send'}
                 />
-
-
-                <View style={{flexDirection: 'row'}}>
-
-                    <View style={{flex: 1, alignItems: 'flex-end', opacity: !ok ? 1 : 0}}>
-                        {renderSimpleButton(
-                            i18n.t("actions.ask_button"),
-                            ()=>this.createAsk(),
-                            {
-                                loading: this.state.isAsking,
-                                style: {alignSelf: 'flex-end'},
-                                disabled: buttonDisabled,
-                                textStyle: {
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    borderWidth: 1,
-                                    borderColor: Colors.white,
-                                    borderRadius: 6,
-                                    padding: 4,
-                                    paddingHorizontal: 6
-                                }
-                            }
-                        )}
-
-                        {<Text style={{opacity: !!this.state.askContent && !notEditable ? 1 : 0,
-                            color: Colors.white,
-                            fontSize: 11,}}>{`${200 - (askContent || "").length}`}</Text>}
-                    </View>
-                </View>
             </View>
-        );
+
+
+            <View style={{flexDirection: 'row'}}>
+
+                {
+                    (this.state.isFocused || !!this.state.askContent) && (
+                        <View style={{flex: 1, marginTop: 15, alignItems: 'flex-end', opacity: !ok ? 1 : 0}}>
+                            {renderSimpleButton(
+                                i18n.t("actions.ask_button"),
+                                () => this.createAsk(),
+                                {
+                                    loading: asking,
+                                    style: {alignSelf: 'flex-end'},
+                                    disabled: buttonDisabled,
+                                    textStyle: {
+                                        color: buttonCollor,
+                                        fontSize: 16,
+                                        borderWidth: 1,
+                                        borderColor: buttonCollor,
+                                        borderRadius: 6,
+                                        padding: 4,
+                                        paddingHorizontal: 6
+                                    }
+                                }
+                            )}
+
+                            {<Text style={{
+                                opacity: !!this.state.askContent && !notEditable ? 1 : 0,
+                                color: buttonCollor,
+                                fontSize: 11,
+                            }}>{`${200 - (askContent || "").length}`}</Text>}
+                        </View>
+                    )
+                }
+            </View>
+        </View>;
+    }
+
+    setFocused(isFocused) {
+        scheduleOpacityAnimation()
+        this.setState({isFocused})
     }
 
     createAsk() {
@@ -106,11 +130,6 @@ export default class AskInput extends Component<Props, State> {
             Alert.alert(i18n.t('actions.ask'), i18n.t('ask.minimal_length'))
             return
         }
-        if (this.state.isAsking) return;
-        this.setState({isAsking: true});
-
-
-
         Api.safeDispatchAction.call(
             this,
             this.props.dispatch,
@@ -135,13 +154,13 @@ export const CREATE_ASK = ApiAction.create("create_ask", "asked for network");
 
 const styles = StyleSheet.create({
     input:{
-        fontSize: 30,
-        lineHeight: 35,
-        textAlign: 'center',
+        fontSize: 20,
+        // lineHeight: 35,
+        // textAlign: 'center',
         fontFamily: SFP_TEXT_BOLD,
         borderRadius: 10,
         margin: 10,
-        color: Colors.white,
+        color: Colors.greyishBrown,
         borderColor: Colors.greyishBrown,
 
     },
