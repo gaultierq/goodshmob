5467c7cbfcc12bb6ca4162eae8c17b86428a68ca
EliotAndres <eliot.andres@eleves.enpc.fr>
2018-06-29 17:59:04 +0200

    Install Maps plugin.
    WARNING: Missing api key for most build types.

--- src/ui/screens/test.js
+++ src/ui/screens/test.js
@@ -4,7 +4,7 @@ import {Clipboard, Dimensions, Image, StyleSheet, Text, TextInput, TouchableOpac
 import {CheckBox} from "react-native-elements"
 import {Navigation} from "react-native-navigation"
 import GTouchable from "../GTouchable"
-
+import MapView from 'react-native-maps'
 
 type Props = {
 };
@@ -22,20 +22,14 @@ export default class TestScreen extends Component<Props, State> {
 
     render() {
         return <View style={{width: '100%', height: '100%', backgroundColor: 'transparent'}} >
-            <GTouchable onPress={()=>{
-                this.props.navigator.showInAppNotification({
-                    screen: "goodsh.InAppNotif", // unique ID registered with Navigation.registerScreen
-                    passProps: {
-                        title: 'ceci est un titre',
-                        subtitle: 'ceci est du contenu, ceci est du contenu, ceci est du contenu, ceci est du contenu, ceci est du contenu, ',
-                    }, // simple serializable object that will pass as props to the in-app notification (optional)
-                    autoDismissTimerSec: 30 // auto dismiss notification in seconds
-                })
-            }
-            }>
-                <Text style={{marginTop: 400}}>press me</Text>
-            </GTouchable>
-            {/*<GSearchBar/>*/}
+            <MapView
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
         </View>
     }
 }
