8b84b5fa3f80bcff933c1f973e33d4f252c85adf
Quentin Gaultier <quentin.gaultier@gmail.com>
2018-07-23 16:05:45 +0200

    dsign v3: colors

--- src/ui/colors.js
+++ src/ui/colors.js
@@ -1,24 +1,42 @@
 export const Colors = Object.freeze({
     darkerBlack: '#101010',
-    black: '#282828',
-    blue: '#54A4E9',
+
+
+    //move this
     facebookBlue: '#3b5998',
-    darkSkyBlue: '#54A4E9',
+
+    blue: '#778dff',
+    darkSkyBlue: '#637aee',
+    lightBlue: '#96a7ff',
+
+    black: '#282828',
     greyishBrown: '#424242',
     brownishGrey: '#5C5C5C',
+
     greyish: '#B1B1B1',
+
     grey3: '#D5D5D5',
     grey4: '#D5D5D5',
+
     greying: '#EBEBEB',
+
     dirtyWhite2: "#F1F1F1",
+
     dirtyWhite: "#F7F7F7",
+
     white: '#FFFFFF',
 
     green: '#01DEB1',
-    darkerGreen: '#01b687',
+    lightGreen: '#26f3c9',
+    darkerGreen: '#02bf98',
+
     pink: 'rgb(220,124,223)',
     grey142: 'rgb(142,142,147)',
+
     orange: '#F98A8A',
+    lightOrange: '#ffa9a9',
+    darkOrange: '#dc6161',
+
 });
 
 
